b3dbe4ccb28d7dd551bc2de9e325ccd0841f7bed
javmencar <javmencar@gmail.com>
2014-10-23 13:59:16 +0200

    Eliminacion comentarios viejos
    Eliminacion de comentarios y simplicacion del  procedimiento de cargar
    el listbox

--- FrmListado.vb
+++ FrmListado.vb
@@ -22,35 +22,12 @@ Public Class FrmListado
         Me.ListView1.View = View.Details
         'Call cargarColumnasListview(Me.ListView1, "Personal")
         Call cargarSoloColumnasPrincipalesListview()
-        Dim categoria As String
         If pos = 0 Then
-            categoria = "Profesor"
+            Call cargarDatosEnListview("Profesores")
         Else
-            categoria = "Alumno"
+            Call cargarDatosEnListview("Alumnos")
         End If
-        Call cargarDatosEnListview(categoria)
-       
-
     End Sub
-    'Private Sub cargarColumnasListview(ByVal lst As ListView, ByVal tabla As String)
-    '    'con esto cargamos todas las columnas del listview
-    '    Try
-    '        Dim sql As String = "Select c.name FROM sys.columns c JOIN sys.tables t ON c.object_id = t.object_id WHERE t.name = '" & tabla & "'"
-    '        cn.Open()
-    '        Dim cmd As New SqlCommand(sql, cn)
-    '        Dim dr As SqlDataReader
-    '        dr = cmd.ExecuteReader
-    '        While dr.Read
-    '            Me.ListView1.Columns.Add(dr(0), 75, HorizontalAlignment.Center)
-    '        End While
-    '    Catch ex2 As miExcepcion
-    '        MsgBox(ex2.ToString)
-    '    Catch ex As Exception
-    '        MsgBox(ex.ToString)
-    '    Finally
-    '        cn.Close()
-    '    End Try
-    'End Sub
     Private Sub cargarSoloColumnasPrincipalesListview()
         Me.ListView1.Columns.Add("Id", 25, HorizontalAlignment.Center)
         Me.ListView1.Columns.Add("DNI", 75, HorizontalAlignment.Center)
@@ -58,60 +35,19 @@ Public Class FrmListado
         Me.ListView1.Columns.Add("Apellido1", 180, HorizontalAlignment.Center)
         Me.ListView1.Columns.Add("Apellido2", 180, HorizontalAlignment.Center)
     End Sub
-    'Private Sub cargarDatosEnListview(ByVal pr As String)
-    '    Try
-    '        cn.Open()
-    '        Dim sql As String
-    '        sql = String.Format("SELECT Personal.{0}, Personal.DNI, Personal.Nombre, Personal.Apellido1, Personal.Apellido2 FROM Personal", pr)
-
-    '        Dim cmd As New SqlCommand(sql, cn)
-    '        Dim dr As SqlDataReader
-    '        dr = cmd.ExecuteReader
-    '        'creo un contador para ayudarme a poner los valores en los items correctos
-    '        Dim i As Integer = 0
-    '        While dr.Read
-    '            ' compruebo el primer valor, si es uno añado el dato
-    '            If dr(0) = True Then
-    '                'aqui añado un dato nuevo
-    '                Me.ListView1.Items.Add(dr(1))
-    '                'aqui añado los subitems al recien añadido
-    '                Me.ListView1.Items(i).SubItems.Add(CStr(dr(2)))
-    '                Me.ListView1.Items(i).SubItems.Add(CStr(dr(3)))
-    '                Me.ListView1.Items(i).SubItems.Add(CStr(dr(4)))
-    '                'aumentamos el contador para la siguiente vuelta
-    '                i += 1
-    '            Else
-    '            End If
-    '        End While
-
-    '    Catch ex2 As miExcepcion
-    '        MsgBox(ex2.ToString)
-    '    Catch ex As Exception
-    '        MsgBox(ex.ToString)
-    '    Finally
-    '        cn.Close()
-    '    End Try
-    'End Sub
-
+  
     Private Sub cargarDatosEnListview(ByVal cat As String)
         Try
             cn.Open()
-            Dim sql As String
-            If cat = "profesor" Then
-                sql = "SELECT Alumnos.id, DatosPersonales.DNI, DatosPersonales.Nombre, DatosPersonales.Apellido1, DatosPersonales.Apellido2" &
-                    " FROM(alumnos, DatosPersonales) WHERE DatosPersonales.Id= Alumnos.IdDP"
-            Else
-                sql = "SELECT Profesores.id, DatosPersonales.DNI, DatosPersonales.Nombre, DatosPersonales.Apellido1, DatosPersonales.Apellido2" &
-                    " FROM(Profesores, DatosPersonales) WHERE DatosPersonales.Id = Profesores.IdDP"
-            End If
+            Dim sql As String = String.Format("SELECT {0}.id, DatosPersonales.DNI, DatosPersonales.Nombre, DatosPersonales.Apellido1, DatosPersonales.Apellido2" &
+                    " FROM({0}, DatosPersonales) WHERE DatosPersonales.Id= A{0}.IdDP", cat)
+            MsgBox(sql)
             Dim cmd As New SqlCommand(sql, cn)
             Dim dr As SqlDataReader
             dr = cmd.ExecuteReader
             'creo un contador para ayudarme a poner los valores en los items correctos
             Dim i As Integer = 0
             While dr.Read
-                ' compruebo el primer valor, si es uno añado el dato
-
                 'aqui añado un dato nuevo
                 Me.ListView1.Items.Add(dr(0))
                 'aqui añado los subitems al recien añadido
@@ -121,10 +57,7 @@ Public Class FrmListado
                 Me.ListView1.Items(i).SubItems.Add(CStr(dr(4)))
                 'aumentamos el contador para la siguiente vuelta
                 i += 1
-                Else
-                End If
             End While
-
         Catch ex2 As miExcepcion
             MsgBox(ex2.ToString)
         Catch ex As Exception
